d319d1518c7a801f83360edbd39fc45faba27350
changicho <changicho29@gmail.com>
2020-07-30 18:01:26 +0900

    feat: 브라우저 라우팅 코드 추가
    Path에 해당하는 모델을 구독하는 router 컴포넌트 추가

---
+++ front/src/router/Path.ts
@@ -0,0 +1,34 @@
+import Observable from "../models/Observable";
+
+class PathModel extends Observable {
+  private $pathName: string;
+
+  constructor() {
+    super();
+
+    const url = new URL(document.URL);
+    const pathName = url instanceof URL ? url.pathname : url;
+
+    this.$pathName = pathName;
+
+    window.addEventListener("popstate", () => {
+      const url = new URL(document.URL);
+      const pathName = url instanceof URL ? url.pathname : url;
+
+      this.changePath(pathName);
+    });
+  }
+
+  pushState(data: unknown, title: string, path: string) {
+    history.pushState(data, title, path);
+
+    this.changePath(path);
+  }
+
+  changePath(path: string) {
+    this.$pathName = path;
+    this.notify(this.$pathName);
+  }
+}
+
+export default new PathModel();
