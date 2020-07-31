b6a87d1867f055527f2697b1dc6a2995c5e33f68
changicho <changicho29@gmail.com>
2020-07-31 12:49:03 +0900

    feat: 현재 월을 변경하는 부분 컴포넌트로 설정
    데이터의 변경과, view의 변경이 일어나므로 별도의 컴포넌트를 분리함
    
    related issue: #4

---
+++ front/src/components/Main/ChangeMonth.ts
@@ -0,0 +1,61 @@
+import Component from "../Component";
+
+import RootModel from "../../models/RootModel";
+
+import "./ChangeMonth.scss";
+
+const month = `<button class="prev">◁</button>
+<p class="month"></p>
+<button class="next">▷</button>`;
+
+export default class ChangeMonth extends Component {
+  private $prevButton: HTMLButtonElement;
+  private $nextButton: HTMLButtonElement;
+  private $month: HTMLParagraphElement;
+
+  constructor() {
+    super();
+
+    this.view = document.createElement("div");
+    this.view.className = "month";
+    this.view.innerHTML = month;
+
+    this.$prevButton =
+      this.view.querySelector("button.prev") ||
+      document.createElement("button");
+    this.$nextButton =
+      this.view.querySelector("button.next") ||
+      document.createElement("button");
+
+    console.log(this.$prevButton, this.$nextButton);
+    this.$month =
+      this.view.querySelector("p.month") || document.createElement("p");
+
+    this.changeMonthText(RootModel.getMonth());
+    this.addListeners();
+    this.subscribes();
+  }
+
+  private addListeners() {
+    this.$prevButton.addEventListener("click", () => {
+      RootModel.prevMonth();
+    });
+
+    this.$nextButton.addEventListener("click", () => {
+      RootModel.nextMonth();
+    });
+  }
+
+  private subscribes() {
+    RootModel.subscribe(
+      "changeMonthParam",
+      (data: { year: number; month: number }) => {
+        this.changeMonthText(data.month);
+      }
+    );
+  }
+
+  private changeMonthText(month: number) {
+    this.$month.textContent = `${month}`;
+  }
+}
