6e74f6b340beef32f511a061a6f38a826cd75561
LimHaksu <limhaksu@naver.com>
2020-07-31 12:55:00 +0900

    fix: historyItemOptions의 데이터 타입 수정
    income, outcome 타입에서 income, amount로 바뀜

--- front/src/components/History/History.ts
+++ front/src/components/History/History.ts
@@ -83,15 +83,6 @@ class History extends Component {
       innerHtml: `1000000`,
     });
 
-    const historyItemOptions = data.map((element) => {
-      const { category, detail, paymentMethod, income, outcome } = element;
-      return {
-        category,
-        detail,
-        paymentMethod,
-        amount: income || outcome || 0,
-      };
-    });
     const historyList = new HistoryList({
       historyItemOptions: data,
     });
