a74f7cd0c0085965bcc74d7335fbd3d242b8bc92
LimHaksu <limhaksu@naver.com>
2020-07-31 10:09:51 +0900

    feat: HistoryDay 컴포넌트 작성
    내역 목록에서 날짜를 구분해주는 영역의 컴포넌트

---
+++ front/src/components/History/HistoryDay.ts
@@ -0,0 +1,41 @@
+import Component, { ComponentOption } from "../Component";
+
+interface HistoryDayOption extends ComponentOption {
+  month: number;
+  day: number;
+  weekDay: string;
+  totalIncome: number;
+  totalOutcome: number;
+}
+
+class HistoryDay extends Component {
+  constructor(option: HistoryDayOption) {
+    super("div", option);
+
+    const { month, day, weekDay, totalIncome, totalOutcome } = option;
+
+    this.setInnerHtml(
+      `<span class="history-day-first">
+    <span class="history-day-month">
+        ${month} 월
+    </span>
+    <span class="history-day-day">
+        ${day} 일
+    </span>
+    <span class="history-day-week-day">
+        ${weekDay}
+    </span>
+</span>
+<span class="history-day-second">
+    <span class="history-day-total-income">
+        +${totalIncome}원
+    </span>
+    <span class="history-day-total-outcome">
+        -${totalOutcome}원
+    </span>
+</span>`
+    );
+  }
+}
+
+export default HistoryDay;
