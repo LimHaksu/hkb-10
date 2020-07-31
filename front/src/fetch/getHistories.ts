6723ce3c66f73b91477646b2ef4f246f6662713e
LimHaksu <limhaksu@naver.com>
2020-07-31 10:35:33 +0900

    feat: 백엔드에 내역을 요청하는 fetch 함수 작성
    api 호출을 관리하기 편하게 하기 위해 한 폴더(fetch 폴더에) 작성
    
    실제로 요청하는 컴포넌트에서는 해당 파일을 import 해서 사용

---
+++ front/src/fetch/getHistories.ts
@@ -0,0 +1,29 @@
+const baseUrl = "http://localhost:3000/api";
+
+interface HistoryDataType {
+  year: number;
+  month: number;
+  day: number;
+  category: string;
+  paymentMethod: string;
+  income?: number;
+  outcome?: number;
+  detail: string;
+}
+
+const getHistories = async (year: number, month: number) => {
+  try {
+    const response = await fetch(`${baseUrl}/histories/${year}/${month}`, {
+      mode: "cors",
+      method: "GET",
+    });
+    const result = await response.json();
+    if (result.success) {
+      return result.data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { getHistories, HistoryDataType };
