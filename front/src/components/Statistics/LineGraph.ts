7b81c2f0731ef63953a97b951f470345369e01a3
changicho <changicho29@gmail.com>
2020-08-03 17:33:15 +0900

    feat: 꺽은선 그래프 컴포넌트 추가
    데이터 fetching 전 더미데이터를 이용해 그리도록 구성함
    꺽은선 그래프는 svg를 이용해 구성하고, animate 태그를 이용해 애니메이션
    을 지정함
    
    공통으로 사용하는 애니메이션 속성은 constant로 관리
    
    svg의 경우 appendChild 등으로 구성하기 어려워, string으로 구성하고
    innerHTML로 렌더링하도록 구성함
    
    related issue: #32

---
+++ front/src/components/Statistics/LineGraph.ts
@@ -0,0 +1,59 @@
+import Component from "../Component";
+import {
+  getStandards,
+  createAverageLine,
+  createDateStandards,
+  createDots,
+  createStandards,
+  createStandardsLine,
+  createPolyLine,
+  GraphSize,
+  DateData,
+} from "./LineGraphFunction";
+import data from "./dummyData";
+
+import "./LineGraph.scss";
+
+export default class LineGraph extends Component {
+  _size: GraphSize;
+
+  constructor(width: number, height: number) {
+    super();
+
+    this._size = {
+      width: width | 800,
+      height: height | 600,
+      top: 20,
+      bottom: 80,
+    };
+
+    const { array, average } = getStandards(data.data);
+    const first = array[0];
+    const last = array[array.length - 1];
+
+    const content = `
+<svg class="graph" width="${this._size.width}" height="${this._size.height}">
+<g class="grid y-grid">
+  <line x1="80" x2="720" y1="${
+    this._size.height - this._size.bottom + 30
+  }" y2="${this._size.height - this._size.bottom + 30}" stroke-width="5"></line>
+  ${createStandardsLine(array, this._size).join("\n")}
+  ${createAverageLine(average, first, last, this._size)}
+</g>
+<g class="labels x-labels">
+  ${createDateStandards(data.year, data.month, this._size).join("\n")}
+</g>
+<g class="labels y-labels">
+  ${createStandards(array, this._size).join("\n")}
+</g>
+<g class="data">
+${createPolyLine(data.data, first, last, this._size)}
+${createDots(data.data, first, last, this._size).join("\n")}
+</g>
+</svg>
+`;
+
+    this.view = document.createElement("div");
+    this.view.innerHTML = content;
+  }
+}
