be4278b06f78d80bd8b9319d5e9944f1c281c2aa
changicho <changicho29@gmail.com>
2020-08-03 17:40:54 +0900

    feat: 꺽은선 그래프를 통계 페이지에 적용함
    ?: 이전에 만든 컴포넌트 적용
    
    related issue: #32
    related commit: 7b81c2f

--- front/src/components/Statistics/index.ts
+++ front/src/components/Statistics/index.ts
@@ -1,10 +1,17 @@
 import Component from "../Component";
+import LineGraph from "./LineGraph";
 
 export default class Statistics extends Component {
+  $lineGraph: LineGraph;
+
   constructor() {
     super();
 
+    this.$lineGraph = new LineGraph(800, 600);
+
     this.view = document.createElement("div");
     this.view.innerText = "statistics";
+
+    this.view.appendChild(this.$lineGraph.view);
   }
 }
