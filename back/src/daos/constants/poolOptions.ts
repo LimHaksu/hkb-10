a842c0110efedd4e47e2e1f3198a3090f7f7b1cf
LimHaksu <limhaksu@naver.com>
2020-08-05 13:58:35 +0900

    feat: 백엔드 env 설정파일 세팅하고 이를 바당으로 pool option 설정
    env 파일로 DB 계정 정보를 관리하기 위함

---
+++ back/src/daos/constants/poolOptions.ts
@@ -0,0 +1,20 @@
+export default {
+  test: {
+    host: process.env.DB_TEST_HOST,
+    port: process.env.DB_TEST_PORT,
+    user: process.env.DB_TEST_USER,
+    password: process.env.DB_TEST_PASSWORD,
+    database: process.env.DB_TEST_NAME,
+    connectionLimit: 30,
+    charset: "utf8mb4",
+  },
+  production: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectionLimit: 30,
+    charset: "utf8mb4",
+  },
+};
