cbfb3e99524b638744c4207c1eb0f24caf9b5bbd
changicho <changicho29@gmail.com>
2020-08-05 17:16:46 +0900

    feat: 통계 페이지 radio 추가
    radio 버튼으로 파이차트, 선 그래프 중 어떤것을 그릴 지 분기 처리
    이를 위해 통계 페이지에서 사용하는 Model을 생성함

--- front/src/components/Statistics/index.ts
+++ front/src/components/Statistics/index.ts
@@ -1,21 +1,57 @@
 import Component from "../Component";
 import LineGraph from "./LineGraph";
+import PieChart from "./PieChart";
+import Checkboxes from "./CheckBoxes";
+
+import StatisticsPageModel, { CASE } from "../../models/StatisticsPageModel";
 
 export default class Statistics extends Component {
   $lineGraph: LineGraph;
+  $pieChart: PieChart;
+  $checkboxes: Checkboxes;
 
   constructor() {
     super();
 
     this.$lineGraph = new LineGraph(800, 600);
+    this.$pieChart = new PieChart(800, 600);
+    this.$checkboxes = new Checkboxes();
 
     this.view = document.createElement("div");
     this.view.innerText = "statistics";
+    this.view.appendChild(this.$checkboxes.view);
 
-    this.view.appendChild(this.$lineGraph.view);
+    switch (StatisticsPageModel.getCase()) {
+      case CASE.LINE: {
+        this.view.appendChild(this.$lineGraph.view);
+        break;
+      }
+      case CASE.PIE: {
+        this.view.appendChild(this.$pieChart.view);
+        break;
+      }
+    }
+
+    StatisticsPageModel.subscribe("changeCase", (data: CASE) => {
+      switch (data) {
+        case CASE.LINE: {
+          this.view.removeChild(this.$pieChart.view);
+          this.view.appendChild(this.$lineGraph.view);
+          this.$lineGraph.reRender();
+          break;
+        }
+        case CASE.PIE: {
+          this.view.removeChild(this.$lineGraph.view);
+          this.view.appendChild(this.$pieChart.view);
+          this.$pieChart.reRender();
+          break;
+        }
+      }
+    });
   }
 
   reRender(): void {
     this.$lineGraph.reRender();
+    this.$pieChart.reRender();
   }
 }
