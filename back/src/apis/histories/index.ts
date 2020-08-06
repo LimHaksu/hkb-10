3d1ffdd95f40af071e7da15efabfbcaa1093018d
changicho <changicho29@gmail.com>
2020-08-06 15:23:45 +0900

    feat: 작성한 달력 & 통계 에서 사용할 미들웨어 적용
    직전 commit에서 작성한 미들웨어들을 적용함
    
    API의 설명은 apidoc의 주석의 형태로 작성함
    
    related commit: 9385c8383a6fe6d320eab19a121e0faea7a1e979

--- back/src/apis/histories/index.ts
+++ back/src/apis/histories/index.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 
 import getDailyHistories from "./getDailyHistories";
+import categoricalDataMiddleware from "./categoricalDataMiddleware";
+import dailyOutcomeMiddleWare from "./dailyOutcome";
+
 import getHistories from "./getHistories";
 
 const historiesRouter = Router();
@@ -17,6 +20,35 @@ const historiesRouter = Router();
  * @apiSuccess {Object} data  월별 하루당 수입 지출 정보
  */
 historiesRouter.get("/daily/:year/:month", getDailyHistories);
+
+/**
+ * @api {get} /histories/outcome/daily/:year/:month  해당 연 월의 일별 지출 합산을 가져옴
+ * @apiName GetDailyOutcomeHistories
+ * @apiGroup History
+ *
+ * @apiParam {Number} year  불러오고자 하는 연도
+ * @apiParam {Number} month  불러오고자 하는 월
+ *
+ * @apiSuccess {boolean} success  호출 성공 여부
+ * @apiSuccess {Array<{date: number, amount: number}>} data  월별 하루당 지출 정보
+ */
+historiesRouter.get("/outcome/daily/:year/:month", dailyOutcomeMiddleWare);
+
+/**
+ * @api {get} /histories/outcome/category/:year/:month  해당 연 월의 일별 지출 합산을 항목별로 가져옴
+ * @apiName GetDailyCategoryHistories
+ * @apiGroup History
+ *
+ * @apiParam {Number} year  불러오고자 하는 연도
+ * @apiParam {Number} month  불러오고자 하는 월
+ *
+ * @apiSuccess {boolean} success  호출 성공 여부
+ * @apiSuccess {Array<{value: number, title: string}>} data  월별 하루당 항목별 지출 정보
+ */
+historiesRouter.get(
+  "/outcome/category/:year/:month",
+  categoricalDataMiddleware
+);
 
 /**
  * @api {get} /histories/:year/:month  해당 연 월의 내역을 전부 가져옴
