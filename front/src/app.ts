beb1b6b818d77284910d5f68f27845cce2539067
LimHaksu <limhaksu@naver.com>
2020-08-07 00:04:32 +0900

    fix: jwt-decode를 이용하여 로그인한 유저의 아이디를 payload에서 가져옴
    나중에 fetch 보낼때 사용하기 위하여 유저 아이디를 로그인 모델에 저장

--- front/src/app.ts
+++ front/src/app.ts
@@ -13,6 +13,10 @@ import Statistics from "./components/Statistics";
 
 import { HISTORY, CALENDAR, STATISTICS } from "./router/PathConstants";
 
+import LoginModel from "./models/LoginModel";
+
+import jwt_decode from "jwt-decode";
+
 import "./stylesheet/main.scss";
 
 router.setComponent("/login", new Login());
@@ -28,6 +32,8 @@ path.subscribe("changePath", (path: string) => {
 function isLoggedIn(): boolean {
   const token = sessionStorage.getItem("token");
   if (token) {
+    const decoded: { user: string; iat: string } = jwt_decode(token);
+    LoginModel.setLoggedInUserId(decoded.user);
     return true;
   }
   return false;
