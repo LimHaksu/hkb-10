68b4f2f5c5e1d403404a54bcd19ecac7704cde29
LimHaksu <limhaksu@naver.com>
2020-08-07 10:39:01 +0900

    fix: payment method dto 에 id를 추가함
    나중에 payment method를 제거할때 id가 필요하므로 추가함

--- back/src/daos/dto/PaymentMethod.ts
+++ back/src/daos/dto/PaymentMethod.ts
@@ -1,8 +1,10 @@
 class PaymentMethod {
+  id: number;
   value: number;
   name: string;
 
-  constructor(value: number, name: string) {
+  constructor(id: number, value: number, name: string, userId?: string) {
+    this.id = id;
     this.value = value;
     this.name = name;
   }
